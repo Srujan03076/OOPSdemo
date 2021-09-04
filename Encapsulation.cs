12388adb7aaf0c1211fa4be67602b8c51d8167f8
unknown <srujanmahaja5@gmail.com>
2021-09-04 12:36:26 +0530

    [Srujan] Add New project

---
+++ Encapsulation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSdemo058
+{
+    class Account
+    {
+
+        int accountBalance = 1000;
+        public void SetBalance(int amount)
+        {
+            accountBalance = amount;
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine("Your Account Balance is" + accountBalance);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.SetBalance(10000);
+            myAccount.GetBalance();
+            Console.ReadLine();
+        }
+    }
+
+}
