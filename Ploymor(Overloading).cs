d4f7718b3e24bf35b6a2f5e0cc3af4776585bc4f
unknown <srujanmahaja5@gmail.com>
2021-09-04 13:05:17 +0530

    [Srujan] Add New project

---
+++ Ploymor(Overloading).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsdemo
+{
+    class Ploymor_Overloading_
+    {
+        static int PlusMethodInt(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double PlusMethodDouble(double x, double y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            int myNum1 = PlusMethodInt(8, 5);
+            double myNum2 = PlusMethodDouble(4.3, 6.26);
+            Console.WriteLine("Int: " + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+        }
+    }
+}
